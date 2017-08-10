30bcc8b18a7f41b96029c52590b495ffd18cfff1
Carlo Santos <carlox7@gmail.com>
2017-08-10 15:08:03 -0700

    created backend

---
+++ frontend/src/component/app/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route} from 'react-router-dom'
+import appStoreCreate from '../../lib/app-store-create.js'
+
+
+const store = appStoreCreate()
+class App extends React.Component {
+  render(){
+    return (
+      <div className='app'>
+        <Provider store={store}>
+          <BrowserRouter>
+            <main>
+              <h1> cool beans </h1>
+            </main>
+          </BrowserRouter>
+        </Provider>
+      </div>
+    )
+  }
+}
+
+export default App
